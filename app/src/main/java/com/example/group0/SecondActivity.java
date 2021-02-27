5ec6ab50d629b292197249810e63407f7fd27378
Shaheenyazid <76759087+Shaheenyazid@users.noreply.github.com>
2021-02-27 17:01:21 +0800

    dh siap dashboard n edit mana yg patut

--- app/src/main/java/com/example/group0/SecondActivity.java
+++ app/src/main/java/com/example/group0/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.group0;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +10,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
-    private Button button;
+
+    CardView Clothes;
+    CardView AboutUs;
+    CardView ContactUs;
+    CardView Logout;
+
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -21,27 +28,52 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        button = (Button) findViewById(R.id.btnClothes);
-        button.setOnClickListener(new View.OnClickListener() {
+        Clothes = findViewById(R.id.cardClothes);
+        AboutUs = findViewById(R.id.cardAbout);
+        ContactUs = findViewById(R.id.cardContact);
+        Logout = findViewById(R.id.cardLogout);
+
+        Clothes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivityclothes();
+                startActivity(new Intent(SecondActivity.this, Clothes.class));
+                showToast("Clothes Clicked");
+            }
+        });
+
+        AboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this, AboutUs.class));
+                showToast("About Us Clicked");
+            }
+        });
+
+        ContactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this, ContactUs.class));
+                showToast("Contact Us Clicked");
+            }
+        });
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
+                showToast("Logout Clicked");
             }
         });
 
         firebaseAuth = FirebaseAuth.getInstance();
-
     }
-        public void openActivityclothes() {
-            Intent intent = new Intent(this, Clothes.class);
-            startActivity(intent);
-        }
 
-    private void Logout(){
-        firebaseAuth.signOut();
-        finish();
-        startActivity(new Intent(SecondActivity.this, MainActivity.class));
-    }
+
+
+
+
 
 
     @Override
@@ -54,14 +86,14 @@ public class SecondActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
 
         switch (item.getItemId()){
-            case R.id.logoutMenu:{
-                Logout();
-                break;
-            }
             case R.id.profileMenu:
                 startActivity(new Intent(SecondActivity.this, ProfileActivity.class));
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
